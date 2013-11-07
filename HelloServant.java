15d707c104c3ca835b051e23aed7c7d84f84a349
Lucas Silvestre <lukas.silvestre@gmail.com>
2013-11-07 10:14:37 +0000

    Implement servant

---
+++ HelloServant.java
@@ -0,0 +1,12 @@
+// The package containing our stubs.
+import HelloApp.*;
+
+//Servant must inherit the generated code
+class HelloServant extends _HelloImplBase {
+	// Add the sayHello method here in the next step.
+
+	public String sayHello() {
+	// Add the method implementation here in the next step.
+	return "\nHello, World!\n";
+	}
+}
